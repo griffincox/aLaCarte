069b2c13706b31e7f6bfc4c6692cf0bc24197311
griffincox <griffincox@gmail.com>
2016-04-12 13:54:21 -0500

    prepend added, working on click line-through

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 $('form').submit(function () {
   //store value of text input
   var value = $('input[type=text]').val();
-  $('ul').append('<li>'+value+'</li>');
+  $('ul').prepend('<li>'+value+'</li>');
   $('input').val('');
   $('input').attr("placeholder", "")
   return false;
+});
+$('li').click(function() {
+  alert:("handler for .click() called");
+  $('li').css("text-decoration", "line-through");
 });
