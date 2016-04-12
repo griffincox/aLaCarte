8630ed0a07ca8061a9945f7f109dcfbdf9070cae
griffincox <griffincox@gmail.com>
2016-04-12 14:40:34 -0500

    still working on strike-through

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $('form').submit(function () {
   //store value of text input
   var value = $('input[type=text]').val();
-  $('ul').prepend('<li>'+value+'</li>')
-  .click(function() {
-    console.log("testing")
-    $(this).css("text-decoration", "line-through")
-  });
+  $('ul').prepend('<li>'+value+'</li>');
   $('input').val('');
-  $('input').attr("placeholder", "")
+  $('input').attr("placeholder", "");
   return false;
+});
+$('li').click(function() {
+  console.log("testing");
+  $(this).css("text-decoration", "line-through")
 });
